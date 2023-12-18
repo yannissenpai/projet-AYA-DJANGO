0691015948ce5f6bc630d16b00a8489ff4bc7059
yannissenpai <132704788+yannissenpai@users.noreply.github.com>
2023-12-18 16:53:43 +0100

    Add files via upload

--- AYA/home/admin.py
+++ AYA/home/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import student
+from .models import subject
+from .models import grade
 
-# Register your models here.
+class Adminstudent(admin.ModelAdmin):
+    list_display = ('name', 'first_name', 'student_code')
+
+    
+
+class Adminsubject(admin.ModelAdmin):
+    list_display = ('name', 'subject_code')
+
+    
+
+class Admingrade(admin.ModelAdmin):
+    list_display = ('name', 'student', 'grade', 'date')
+
+    
+
+
+admin.site.register(student, Adminstudent)
+admin.site.register(subject, Adminsubject)
+admin.site.register(grade, Admingrade)
--- AYA/home/models.py
+++ AYA/home/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class student(models.Model):
+	name = models.CharField(max_length=150)
+	first_name = models.CharField(max_length=150)
+	student_code = models.CharField(max_length=150, unique=True)
+
+	def __str__(self):
+		return self.name
+	
+
+class subject(models.Model):
+	name = models.CharField(max_length=150)
+	subject_code = models.CharField(max_length=150, unique=True)
+
+	def __str__(self):
+		return self.name
+
+class grade(models.Model):
+	name = models.ForeignKey(subject, on_delete=models.CASCADE)
+	student = models.ForeignKey(student, on_delete=models.CASCADE)
+	grade = models.DecimalField(max_digits=3, decimal_places=1)
+	date = models.DateField(auto_now=True)
+
+	
+
+
+	
+	"""
+	Pour chaque modif de ma table ou classe faire:
+	python manage.py makemigrations
+	python manage.py migrate
+	"""
