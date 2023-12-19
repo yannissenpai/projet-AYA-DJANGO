cfcd5eefc77200eee6d2e98de1b7296ff5e995d8
yannissenpai <132704788+yannissenpai@users.noreply.github.com>
2023-12-19 01:36:46 +0100

    Add files via upload

--- AYA/home/views.py
+++ AYA/home/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from .models import grade
+
+def home(request, *args, **kwargs):
+    grades = grade.objects.all()
+    contexte = {
+    'name':grade.student
+    }
+   
 
 
+    return render(request, 'home/index.html', )
+
+
+
+
+"""
 def index(request):
     return render(request, 'home/index.html')
+    """
