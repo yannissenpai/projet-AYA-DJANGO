74e9559d0979d2879eb7211bc764aa8db2f647a5
AhmedSouai <59059201+AhmedSouai@users.noreply.github.com>
2023-12-22 17:28:33 +0100

    v.1.0

--- AYA/home/views.py
+++ AYA/home/views.py
@@ -1,6 +1,10 @@
+from multiprocessing import context
 from django.shortcuts import render
-from .models import student
+from .models import student, grade
 from .form import CreatStudent
+from reportlab.pdfgen import canvas
+from django.http import HttpResponse
+
 
 def test(request, *args, **kwargs):
     students = student.objects.all()
@@ -17,13 +21,35 @@ def createS(request):
         form = CreatStudent()
         message = '+1 Student added'
 
-
     return render(request, 'home/detailcreateS.html', {'form':form, 'message':message,})
 
+def gpdf(request):
+    message = ""
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        try:
+            student_instance = student.objects.get(name=name)
+            grades = grade.objects.filter(student=student_instance)
+            
+            # Création de la réponse HTTP de type PDF
+            response = HttpResponse(content_type='application/pdf')
+            response['Content-Disposition'] = f'attachment; filename="{name}_grades.pdf"'
 
+            # Création d'un objet canvas pour dessiner le PDF
+            p = canvas.Canvas(response)
 
+            # Écriture des données dans le PDF
+            y_position = 800
+            for g in grades:
+                p.drawString(100, y_position, f"Nom de l'étudiant: {g.student.name}, Matière: {g.name.name}, Note: {g.grade}, Date: {g.date}")
+                y_position -= 40
 
-"""
-def index(request):
-    return render(request, 'home/index.html')
-    """
+            # Finalisation du PDF
+            p.showPage()
+            p.save()
+            return response
+        except student.DoesNotExist:
+            message = "Utilisateur introuvable"
+
+    # Rendu du template avec un message d'erreur le cas échéant
+    return render(request, 'home/gpdf.html', {'message': message})
